7f78121b7541ca2b2669dec861661c0064d86f55
Georgia714 <71225711+GeorgiaD714@users.noreply.github.com>
2021-03-01 11:40:16 +0000

    Update hw.js

--- hw.js
+++ hw.js
@@ -24,29 +24,6 @@ let a = 2;
 let x = 1 + (a*=2);
 console.log(a, x);*/
 
-//FAHRENHEIT-CELSIUS EXERCISE, slide 40
-
-//OPTION 1 
-/*let userFahrenheit = prompt("Enter temperature in fahrenheit: ") 
-let celsius = (userFahrenheit - 32) * (5/9);
-console.log(Math.floor(celsius));
-let userCelsius = prompt("Enter temperature in celsius: ")
-let fahrenheit = userCelsius / (5/9) + 32;
-console.log(Math.floor(fahrenheit));*/
-
-//OPTION 2
-/*let userTemperature = prompt("Enter a temperature: ")
-let tempUnit = prompt("Enter the unit of temperature (Celsius or Fahrenheit: ")
-if (tempUnit === 'celsius') {
-    let celsius = Math.floor(userTemperature - 32 * (5/9));
-    console.log('The temperature in degrees celsius is: ' + celsius)
-}
-else {
-    let fahrenheit = Math.floor(userTemperature / (5/9) + 32);
-    console.log('The temperature in degrees fahrenheit is: ' + fahrenheit)
-};*/
-
-
 
 
 

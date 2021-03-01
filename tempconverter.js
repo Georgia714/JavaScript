0b630ea571e29ec8d1e999f3d5ed48beaf124d3a
Georgia714 <71225711+GeorgiaD714@users.noreply.github.com>
2021-03-01 11:41:28 +0000

    Create tempconverter.js

---
+++ tempconverter.js
@@ -0,0 +1,21 @@
+//This file contains code to convert a temperature entered in celsius into fahrenheit and vice versa, and prints the result to the console. It gives two options.
+
+//OPTION 1 
+/*let userFahrenheit = prompt("Enter temperature in fahrenheit: ") 
+let celsius = (userFahrenheit - 32) * (5/9);
+console.log(Math.floor(celsius));
+let userCelsius = prompt("Enter temperature in celsius: ")
+let fahrenheit = userCelsius / (5/9) + 32;
+console.log(Math.floor(fahrenheit));*/
+
+//OPTION 2
+/*let userTemperature = prompt("Enter a temperature: ")
+let tempUnit = prompt("Enter the unit of temperature (Celsius or Fahrenheit: ")
+if (tempUnit === 'celsius') {
+    let celsius = Math.floor(userTemperature - 32 * (5/9));
+    console.log('The temperature in degrees celsius is: ' + celsius)
+}
+else {
+    let fahrenheit = Math.floor(userTemperature / (5/9) + 32);
+    console.log('The temperature in degrees fahrenheit is: ' + fahrenheit)
+};*/
